5284eb47029f5750fe79ca65ac42202aabab11bc
Anand Krishnan <anandkp92@gmail.com>
2018-10-25 14:07:38 -0700

    remove the group toggle after creating a group

--- tled_zigbee/group.py
+++ tled_zigbee/group.py
@@ -8,9 +8,9 @@ controller = Controller(config_file='settings.yaml')
 #controller.start()
 #controller.permit_joining(interval=0xf0)
 
-#controller.add_groups(light_address=0xbacb, light_ep=0x40, group_address=0xaaad)
-#controller.add_groups(light_address=0x1b97, light_ep=0x40, group_address=0xaaad)
+controller.add_groups(light_address=0xbacb, light_ep=0x40, group_address=0xaaad)
+controller.add_groups(light_address=0x1b97, light_ep=0x40, group_address=0xaaad)
 
 
-group = Light(0xaaad, 0x40, controller, is_group=True)
-group.toggle()
+# group = Light(0xaaad, 0x40, controller, is_group=True)
+# group.toggle()
