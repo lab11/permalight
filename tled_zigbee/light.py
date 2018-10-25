5d7776229c2ad162e094c92a319b3610919b5d7b
Anand Krishnan Prakash <anandkp92@gmail.com>
2018-10-24 17:23:12 -0700

    adding a delay after executing a control command and adding timeouts for synchronous read

--- tled_zigbee/light.py
+++ tled_zigbee/light.py
@@ -1,8 +1,9 @@
 from utils import PARITY_CONSTANTS, BYTE_SIZE_CONSTANTS, STOP_BITS_CONSTANTS
 import utils
+import time
 
 class Light():
-  def __init__(self, address, endpoint, controller, is_group=False):
+  def __init__(self, address, endpoint, controller, is_group=False, read_timeout = 3):
     self.address = address
     self.endpoint = endpoint
 
@@ -14,7 +15,9 @@ class Light():
     self.level = -1
 
     self.controller = controller
+    self.last_controlled_time = time.time()
     self.device = self.controller.get_device()
+    self.read_timeout = read_timeout
 
   def get_on_off_toggle_data(self, command):
 
@@ -41,78 +44,87 @@ class Light():
     data = [msg_type_msb, msg_type_lsb, length_msb, length_lsb, checksum] + content
     return data
 
-  def off(self, print_return=False):
-    self.state = 0
-    data = self.get_on_off_toggle_data(command=0x0)
-    bytes_to_send = utils.create_byte_stream_to_send(data_bytes=data)
-    self.device.write(bytes_to_send)
-    if print_return:
-      rsp = [self.synchronous_read(), self.synchronous_read()]
-      print (rsp)
+  def off(self, print_return=False, seconds_before_next_command=2):
+  	if time.time() - self.last_controlled_time > seconds_before_next_command:
+	    self.state = 0
+	    data = self.get_on_off_toggle_data(command=0x0)
+	    bytes_to_send = utils.create_byte_stream_to_send(data_bytes=data)
+	    self.device.write(bytes_to_send)
+	    self.last_controlled_time = time.time()
+	    if print_return:
+	      rsp = [self.synchronous_read()]
+	      print (rsp)
 
-  def on(self, print_return=False):
-    self.state = 1
-    data = self.get_on_off_toggle_data(command=0x01)
-    bytes_to_send = utils.create_byte_stream_to_send(data_bytes=data)
-    self.device.write(bytes_to_send)
-    if print_return:
-      rsp = [self.synchronous_read(), self.synchronous_read()]
-      print (rsp)
+  def on(self, print_return=False, seconds_before_next_command=2):
+  	if time.time() - self.last_controlled_time > seconds_before_next_command:
+	    self.state = 1
+	    data = self.get_on_off_toggle_data(command=0x01)
+	    bytes_to_send = utils.create_byte_stream_to_send(data_bytes=data)
+	    self.device.write(bytes_to_send)
+	    self.last_controlled_time = time.time()
+	    if print_return:
+	      rsp = [self.synchronous_read()]
+	      print (rsp)
 
-  def toggle(self, print_return=False):
-    self.state = not self.state
-    data = self.get_on_off_toggle_data(command=0x02)
-    bytes_to_send = utils.create_byte_stream_to_send(data_bytes=data)
-    self.device.write(bytes_to_send)
-    if print_return:
-      rsp = [self.synchronous_read(), self.synchronous_read()]
-      print (rsp)
+  def toggle(self, print_return=False, seconds_before_next_command=2):
+  	if time.time() - self.last_controlled_time > seconds_before_next_command:
+	    self.state = not self.state
+	    data = self.get_on_off_toggle_data(command=0x02)
+	    bytes_to_send = utils.create_byte_stream_to_send(data_bytes=data)
+	    self.device.write(bytes_to_send)
+	    self.last_controlled_time = time.time()
+	    if print_return:
+	      rsp = [self.synchronous_read()]
+	      print (rsp)
 
-  def set_level(self, level, transition_time=1, with_on_off=True, print_return=False):
+  def set_level(self, level, transition_time=1, with_on_off=True, print_return=False, seconds_before_next_command=2):
 
-    self.level = level
-    msg_type = 0x0081
-    msg_type_list = utils.convert_int16_msb_lsb_list(msg_type)
-    msg_type_msb = msg_type_list['msb']
-    msg_type_lsb = msg_type_list['lsb']
+	if time.time() - self.last_controlled_time > seconds_before_next_command:
+	    self.level = level
+	    msg_type = 0x0081
+	    msg_type_list = utils.convert_int16_msb_lsb_list(msg_type)
+	    msg_type_msb = msg_type_list['msb']
+	    msg_type_lsb = msg_type_list['lsb']
 
-    address_mode = self.address_mode
-    destn_list = utils.convert_int16_msb_lsb_list(self.address)
-    destn_msb = destn_list['msb']
-    destn_lsb = destn_list['lsb']
+	    address_mode = self.address_mode
+	    destn_list = utils.convert_int16_msb_lsb_list(self.address)
+	    destn_msb = destn_list['msb']
+	    destn_lsb = destn_list['lsb']
 
-    source_ep = 0x01
+	    source_ep = 0x01
 
-    on_off = int(with_on_off)
-    if int(level/100*0xFF) > 0xFF or level < 0x0:
-      print ("out of bounds level : %d. 0x0<=level<=0xff", level)
-      return -1
+	    on_off = int(with_on_off)
+	    if int(level/100*0xFF) > 0xFF or level < 0x0:
+	      print ("out of bounds level : %d. 0x0<=level<=0xff", level)
+	      return -1
 
-    transition_time_list = utils.convert_int16_msb_lsb_list(transition_time)
-    transition_time_msb = transition_time_list['msb']
-    transition_time_lsb = transition_time_list['lsb']
+	    transition_time_list = utils.convert_int16_msb_lsb_list(transition_time)
+	    transition_time_msb = transition_time_list['msb']
+	    transition_time_lsb = transition_time_list['lsb']
 
-    content = [address_mode, destn_msb, destn_lsb, source_ep, self.endpoint, on_off, int(level/100*0xFF), transition_time_msb, transition_time_lsb]
+	    content = [address_mode, destn_msb, destn_lsb, source_ep, self.endpoint, on_off, int(level/100*0xFF), transition_time_msb, transition_time_lsb]
 
-    length = utils.convert_int16_msb_lsb_list(len(content))
-    length_msb = length['msb']
-    length_lsb = length['lsb']
+	    length = utils.convert_int16_msb_lsb_list(len(content))
+	    length_msb = length['msb']
+	    length_lsb = length['lsb']
 
-    checksum = utils.get_checksum(msg_type_msb=msg_type_msb, msg_type_lsb=msg_type_lsb, length_msb=length_msb, length_lsb=length_lsb, data=content)
+	    checksum = utils.get_checksum(msg_type_msb=msg_type_msb, msg_type_lsb=msg_type_lsb, length_msb=length_msb, length_lsb=length_lsb, data=content)
 
-    data = [msg_type_msb, msg_type_lsb, length_msb, length_lsb, checksum] + content
-    bytes_to_send = utils.create_byte_stream_to_send(data_bytes=data)
-    self.device.write(bytes_to_send)
+	    data = [msg_type_msb, msg_type_lsb, length_msb, length_lsb, checksum] + content
+	    bytes_to_send = utils.create_byte_stream_to_send(data_bytes=data)
+	    self.device.write(bytes_to_send)
+	    self.last_controlled_time = time.time()
 
-    if print_return:
-      rsp = [self.synchronous_read(), self.synchronous_read()]
-      print (rsp)
+	    if print_return:
+	      rsp = [self.synchronous_read(), self.synchronous_read()]
+	      print (rsp)
 
   def synchronous_read(self):
     line = self.device.read()
     read_bytes = []
 
-    while line != b'\x03':
+    st = time.time()
+    while line != b'\x03' and time.time() - st < self.read_timeout:
       read_bytes.append(line)
       line = self.device.read()
 
