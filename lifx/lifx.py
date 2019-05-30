62a4b439dbd6f2a0206b47924e1cffa7285a9dcc
Neal Jackson <nealjack@umich.edu>
2019-05-30 14:49:08 -0700

    add retry for all lifxlan commands

--- lifx/lifx.py
+++ lifx/lifx.py
@@ -24,20 +24,26 @@ class Light():
         self.light = context
     def __str__(self):
         return(self.name)
+    @retry(stop_max_attempt_number=5, wait_random_min=1000, wait_random_max=2000)
     def off(self):
         self.light.set_power(0)
         self.light.set_power(0)
+    @retry(stop_max_attempt_number=5, wait_random_min=1000, wait_random_max=2000)
     def on(self):
         self.light.set_power(1)
+    @retry(stop_max_attempt_number=5, wait_random_min=1000, wait_random_max=2000)
     def toggle(self):
         if self.light.get_power() > 0:
             self.light.set_power(0)
         else: self.light.set_power(1)
+    @retry(stop_max_attempt_number=5, wait_random_min=1000, wait_random_max=2000)
     def get_state(self):
         return self.light.get_power() > 0
+    @retry(stop_max_attempt_number=5, wait_random_min=1000, wait_random_max=2000)
     def get_level(self):
         color = self.light.get_color()
         return color[2]/65535.0
+    @retry(stop_max_attempt_number=5, wait_random_min=1000, wait_random_max=2000)
     def set_level(self, level, transition_time=1000):
         if level > 1:
             level = 1
